852dfd6ee8fa125b8ec440606ffe51295ba6f178
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-07 02:19:42 +0100

    8 by 8 double

--- main.ts
+++ main.ts
@@ -11,6 +11,61 @@ input.onButtonPressed(Button.A, function () {
         kitronik_VIEW128x64.clear()
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    kitronik_VIEW128x64.clear()
+    basic.pause(1000)
+    kitronik_VIEW128x64.writeImageDouble(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . . # # . . .
+        . . . # # . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        `), 32, 16)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageDouble(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . . . . . . . .
+        . . # # # # . .
+        . . # . . # . .
+        . . # . . # . .
+        . . # # # # . .
+        . . . . . . . .
+        . . . . . . . .
+        `), 32, 16)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageDouble(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . # # # # # # .
+        . # . . . . # .
+        . # . . . . # .
+        . # . . . . # .
+        . # . . . . # .
+        . # # # # # # .
+        . . . . . . . .
+        `), 32, 16)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageDouble(kitronik_VIEW128x64.matrix8x8(`
+        # # # # # # # #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # # # # # # # #
+        `), 32, 16)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+})
 function Intro () {
     kitronik_VIEW128x64.clear()
     kitronik_VIEW128x64.refresh()
@@ -29,18 +84,73 @@ function Intro () {
     kitronik_VIEW128x64.refresh()
     basic.pause(1000)
 }
+input.onButtonPressed(Button.B, function () {
+    kitronik_VIEW128x64.clear()
+    basic.pause(1000)
+    kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . . # # . . .
+        . . . # # . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        `), 64, 32)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . . . . . . . .
+        . . # # # # . .
+        . . # . . # . .
+        . . # . . # . .
+        . . # # # # . .
+        . . . . . . . .
+        . . . . . . . .
+        `), 64, 32)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix8x8(`
+        . . . . . . . .
+        . # # # # # # .
+        . # . . . . # .
+        . # . . . . # .
+        . # . . . . # .
+        . # . . . . # .
+        . # # # # # # .
+        . . . . . . . .
+        `), 64, 32)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix8x8(`
+        # # # # # # # #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # . . . . . . #
+        # # # # # # # #
+        `), 64, 32)
+    kitronik_VIEW128x64.refresh()
+    basic.pause(1000)
+})
 let list: Image[] = []
 Intro()
 list = [
-images.iconImage(IconNames.Heart),
+kitronik_VIEW128x64.iImage(IconNames.Heart),
 images.iconImage(IconNames.SmallHeart),
-images.iconImage(IconNames.Yes),
+kitronik_VIEW128x64.iImage(IconNames.Yes),
 images.iconImage(IconNames.No),
-images.iconImage(IconNames.Happy),
+kitronik_VIEW128x64.iImage(IconNames.Happy),
 images.iconImage(IconNames.Sad),
-images.iconImage(IconNames.Confused),
+kitronik_VIEW128x64.iImage(IconNames.Confused),
 images.iconImage(IconNames.Angry),
-images.iconImage(IconNames.Asleep),
+kitronik_VIEW128x64.iImage(IconNames.Silly),
 images.iconImage(IconNames.Surprised),
 images.iconImage(IconNames.Silly),
 images.iconImage(IconNames.Fabulous),
@@ -75,6 +185,3 @@ images.iconImage(IconNames.Scissors),
 images.iconImage(IconNames.SmallSquare),
 images.iconImage(IconNames.Square)
 ]
-basic.forever(function () {
-	
-})
